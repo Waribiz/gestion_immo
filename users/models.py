9b4392389bc23827c1c47ed66367ff34d68bc0bd
Waribiz <waribiz680@gmail.com>
2025-04-30 18:00:22 +0100

    MODIFICATIONS DE LA STRUCTURE DU MODEL Users

--- users/models.py
+++ users/models.py
@@ -2,5 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    name= models.CharField(max_length=50, default=None)
     is_client = models.BooleanField(default=True)  # Par défaut c'est un client
     is_admin = models.BooleanField(default=False)  # L'admin sera défini manuellement
--- users/serializers.py
+++ users/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_client', 'is_admin']
+        fields = ['id', 'username', 'name', 'email', 'password', 'is_client', 'is_admin']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -13,10 +13,9 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
             email=validated_data['email'],
             password=validated_data['password'],
+            name=validated_data.get('name'),
             is_client=validated_data.get('is_client', True),
             is_admin=validated_data.get('is_admin', False)
         )
